70b062de17b2e0cdb34b358b9a7626f527219b1e
Antonne Tulloch <gmkalonji@hotmail.com>
2018-06-12 20:45:12 -0500

    Initial commit to github

---
+++ CNGMadeEasy/CNGWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CNGMadeEasy
+{
+    public class CNGWrapper
+    {
+        public string Encrypt(string plainTestPassword, string keyName)
+        {
+            CngKey key;
+            if (CngKey.Exists(keyName))
+                key = CngKey.Open(keyName);
+            else
+                key = CngKey.Create(CngAlgorithm.Rsa, keyName,
+                    new CngKeyCreationParameters { ExportPolicy = CngExportPolicies.AllowPlaintextArchiving });
+
+            var cng = new RSACng(key);
+            
+            var encryptedData = cng.Encrypt(Encoding.UTF8.GetBytes(plainTestPassword), 
+                RSAEncryptionPadding.Pkcs1);
+
+            var encodedData = Convert.ToBase64String(encryptedData);
+            return encodedData;
+        }
+
+        public string Decrypt(string encryptedData, string keyName)
+        {
+            var key = CngKey.Open(keyName);
+            var decodedData = Convert.FromBase64String(encryptedData);
+            var cng = new RSACng(key);
+
+            var decryptedData = cng.Decrypt(decodedData, RSAEncryptionPadding.Pkcs1);
+            var decodedString = Encoding.UTF8.GetString(decryptedData);
+
+            return decodedString;
+        }
+    }
+}
---
+++ CNGMadeEasyTests/CNGWrapperTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CNGMadeEasy.Tests
+{
+    [TestClass()]
+    public class CNGWrapperTests
+    {
+        private string keyName = "MyKey";
+
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            //arrange
+            string stringToEncrypt = "password";
+            string encryptedString = "Updrxe9epasXAPOjsk8hjuKpbAhZ4FLryk8pDcGk5oibQLTgtE/vckPKy1LeeCzNlSwYx2DCpawh15sVERUGNV5LTw1xM14JgHY1NqpJ8M0C8IDjG9chR9A/h1QSMB1OvFmKVWFKM3UlTdzwtvRKYorPRTxmodiP+3EBwehlfrI=";
+
+            //act
+            var cng = new CNGWrapper();
+            var result = cng.Encrypt(stringToEncrypt, keyName);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result, encryptedString, false);
+        }
+
+        [TestMethod]
+        public void DecryptTest()
+        {
+            //arrange
+            string encryptedString = "Updrxe9epasXAPOjsk8hjuKpbAhZ4FLryk8pDcGk5oibQLTgtE/vckPKy1LeeCzNlSwYx2DCpawh15sVERUGNV5LTw1xM14JgHY1NqpJ8M0C8IDjG9chR9A/h1QSMB1OvFmKVWFKM3UlTdzwtvRKYorPRTxmodiP+3EBwehlfrI=";
+            string expectedResult = "password";
+
+            //act
+            var cng = new CNGWrapper();
+            var result = cng.Decrypt(encryptedString, keyName);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result, expectedResult, false);
+        }
+    }
+}
